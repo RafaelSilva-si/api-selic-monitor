e5507bad6de79474527233d976ff3c7588460d32
Rafael Silva <rafaelkaperski@gmail.com>
2025-04-09 15:25:23 -0300

    feat: create fetch selic with puppeteer

---
+++ src/services/fetch-selic.service.ts
@@ -0,0 +1,38 @@
+import puppeteer from 'puppeteer';
+
+export async function fetchSelicValue(): Promise<string> {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox'], // necessário Para o Docker
+  });
+
+  // Indica que vai executar uma nova pagina do browser
+  const page = await browser.newPage();
+
+  // Exemplo de página que contém o valor da Selic
+  await page.goto('https://www.bcb.gov.br/');
+
+  const selicValue = await page.evaluate(() => {
+    // Bloco de referência onde se encontra os valores das taxas
+    const blocos = Array.from(document.querySelectorAll('div.d-flex.align-items-center.justify-content-center'));
+    
+    // Percorre todos esses blocos e pesquisam por qual tem o texto Meta Selic
+    for (const bloco of blocos) {
+    const texto = (bloco as HTMLElement).innerText.toLowerCase();
+      if (texto.includes('meta selic')) {
+        const percentual = bloco.querySelector('.percentual');
+        return percentual?.textContent?.trim() || null;
+      }
+    }
+
+    return null;
+  });
+
+  await browser.close();
+
+  if (!selicValue) {
+    throw new Error('Não foi possível obter o valor da Selic');
+  }
+
+  return selicValue;
+}
