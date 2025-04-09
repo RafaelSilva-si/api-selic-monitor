3fa271b52b68a976f95ab5003de43da44c0e5432
Rafael Silva <rafaelkaperski@gmail.com>
2025-04-09 15:42:37 -0300

    feat: create api to send selic notification

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Request, Response } from 'express';
+
+import { fetchSelicValue } from './services/fetch-selic.service';
+import { sendSelicAlertEmail } from './services/send-email.service';
 
 dotenv.config();
 
@@ -8,8 +12,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const teste = await fetchSelicValue()
+  console.log(teste)
   res.json({ message: 'API Selic Monitor com TypeScript 🎯' });
+});
+
+app.post('/selic/notify', async (req: Request, res: Response): Promise<any> => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ success: false, message: 'Email é obrigatório' });
+  }
+
+  try {
+    const selic = await fetchSelicValue();
+    await sendSelicAlertEmail(selic, email);
+    res.status(200).json({ success: true, selic });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ success: false, error: err.message });
+  }
 });
 
 export default app;
